33ef0d88b725a88b7d8cbe5e0f5f51adbad8d252
tarang369 <tarang369666@gmail.com>
2020-05-29 03:09:24 +0530

    local chat with live typing message

--- index.js
+++ index.js
@@ -13,4 +13,11 @@ app.use(express.static('public'));
 const io = socket(server);
 io.on('connection', (socket) => {
   console.log("made socket connection")
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data)
+  })
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data)
+
+  })
 })
